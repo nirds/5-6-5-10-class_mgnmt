53f283671437f524104e018ff2fedbb1c92a463e
Renée De Voursney <renee@nird.us>
2013-05-08 11:40:20 -0400

    created a new rails app and are now saving a baseline with git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClassMngmt::Application.config.secret_token = '060af3942ad24a8c2740f19f1d83f888d1354713a463a3bf1b577fb3820d2c1a0b25a9e2a4bf54f121805ad429a87e508848df521a50ea48132b2c9d932b9926'
