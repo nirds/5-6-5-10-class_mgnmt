72cf9e2a5c60b3129dc674099d51310133c7a4e8
Renée De Voursney <renee@nird.us>
2013-05-09 10:57:23 -0400

    learning about testing and validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,16 @@ class Student < ActiveRecord::Base
   attr_accessible :age, :bio, :full_name, :title, :email
   has_many :student_courses
   has_many :courses#, :through => :student_courses
+
+  validates_presence_of :email
+  validates :email, format:  { with: /\w*@\w*.\w*/,
+    message: "is not valid, must contain an @" }
+
+  before_create :set_defaults
+
+private
+  def set_defaults
+    self.title = "N/A" if title.blank?
+  end
+
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Student do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before :each do
+    @invalid_student = Student.new
+  end
+
+  it "should require an email" do
+    @invalid_student.should_not be_valid
+  end
+
+  it "should require a valid email" do
+    @invalid_student.email = "badEmail"
+    @invalid_student.valid?.should be_false
+    @invalid_student.errors.full_messages.should include "Email is not valid, must contain an @"
+  end
+
 end
