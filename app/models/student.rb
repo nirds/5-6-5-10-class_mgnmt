8cab4dd5832bf53161c10d910397fc300bdd20f4
Renée De Voursney <renee@nird.us>
2013-05-08 22:24:37 -0400

    add email to student to learn about adding migrations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ActiveRecord::Base
-  attr_accessible :age, :bio, :full_name, :title
+  attr_accessible :age, :bio, :full_name, :title, :email
   has_many :student_courses
   has_many :courses#, :through => :student_courses
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130508195557) do
+ActiveRecord::Schema.define(:version => 20130508203214) do
 
   create_table "courses", :force => true do |t|
     t.string   "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20130508195557) do
     t.text     "bio"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "email"
   end
 
 end
